5fecf2739e3c0f13df9e2031d64743ea0615155a
b0jko3 <bozhidar.kayryakov@abv.bg>
2016-04-19 17:00:35 +0300

    Added files via upload

---
+++ Age.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Age
+{
+    static void Main()
+    {
+        DateTime date = DateTime.Today;
+
+        string birthday = Console.ReadLine();
+        int inputYear = int.Parse(birthday.Substring(6, 4));
+        int inputMonth = int.Parse(birthday.Substring(0, 2));
+        int age = date.Year - inputYear;
+        if (inputMonth > date.Month)
+        {
+            age--;
+        }
+
+        int ageAfterTenYears = age + 10;
+
+        Console.WriteLine(age);
+        Console.WriteLine(ageAfterTenYears);
+    }
+}
