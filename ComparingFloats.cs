ba646248d74e2302d8154865395c70aabb045f16
b0jko3 <bozhidar.kayryakov@abv.bg>
2016-04-19 17:16:24 +0300

    Added files via upload

---
+++ ComparingFloats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class ComparingFloats
+{
+    static void Main()
+    {
+        var first = decimal.Parse(Console.ReadLine());
+        var second = decimal.Parse(Console.ReadLine());
+
+        if (((second - first) == 0.000001m) || (first - second) == 0.000001m)
+        {
+            Console.WriteLine("false");
+        }
+        else if ((first * -1) == (second) || (first == (second * -1)) || (float)first == (float)second)
+        {
+            Console.WriteLine("true", true, (first - second) * -1);
+        }
+        else
+        {
+            Console.WriteLine("false", false, second - first);
+        }
+    }
+}
---
+++ PrinttheASCIITable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class PrinttheASCIITable
+{
+    static void Main()
+    {
+        for (int i = 33; i < 127; i++)
+        {
+            Console.Write((char)i);
+        }
+        Console.WriteLine("");
+    }
+}
